f1cdbc1f9de4936b04f26c0c526a88dc8a4fcf3b
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-09-02 01:27:15 +0300

    Update sample02.dart

--- codeandart/dart01/sample02.dart
+++ codeandart/dart01/sample02.dart
@@ -16,4 +16,43 @@ void main() {
 
   var toup = (value) => '${value.toUpperCase()}';
   print(toup('codeandart'));
+
+  // Named parameters
+  String getModel({String title, int value}) {
+    return '$title - ${value + 10}';
+  }
+  print(getModel(title: 'Model', value: 22));
+
+  // Positioned paremeters
+  String hello(String name, String msg, [String device]) {
+    var result = '$name says $msg';
+
+    if (device != null) {
+      result = '$result from $device';
+    }
+
+    return result;
+  }
+
+  print(hello('Alex', 'Hey'));
+  print(hello('Alex', 'Hey', 'iPhone 100x'));
+
+  // Default parameters
+  String trueName({String name = 'Alex', bool correct = false}) {
+    return correct ? name : 'Wrong name';
+  }
+
+  print(trueName());
+  print(trueName(correct: true));
+
+  String hello2(String name, String msg, [String device = 'Android X']) {
+    var result = '$name says $msg';
+    if (null != device) {
+      result = '$result from $device';
+    }
+    return result;
+  }
+  print(hello2('Alex', 'Hey'));
+  print(hello2('Alex', 'Hey', 'iPhone 10'));
+  
 }
