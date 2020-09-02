2ac266c7f451f835c849b32b8284e6d4e3ba6dad
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-09-02 15:56:27 +0300

    .

---
+++ codeandart/dart01/sample05.dart
@@ -0,0 +1,29 @@
+void main() {
+  // Collections
+  var list = [1,2,3];
+
+  List<int> values = [1,2,3,4,5,6,7,8];
+  values.add(9);
+  values.remove(3);
+
+  print('${values} - size ${values.length}');
+
+  List<int> constantList = const [1,2,3,4,5];
+  // constantList.add(6);
+
+  var list1 = [3,2,1];
+  var list2 = [4, ...list1];
+  print(list2);
+
+  var nullableList;
+  var list3 = [1, ...?nullableList];
+  print(list3);
+
+  bool permission = true;
+  var nav = ['home', 'about', if (permission) 'admin'];
+  print(nav);
+
+  List<int> intList = [1,2,3,4,5];
+  List<String> strList = ['>0', for(var i in intList) '>$i'];
+  print(strList);
+}
