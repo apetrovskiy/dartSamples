b566dd404503b6469e58bb8fdffaffe4166c52a6
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-09-05 22:55:46 +0300

    .

--- codelabs-java/sample007.dart
+++ codelabs-java/sample007.dart
@@ -1,9 +1,7 @@
-// Imperative
+// Functional
 String scream(int length) => "A${'a' * length}h!";
 
 main() {
   final values = [1, 2, 3, 5, 10, 50];
-  for (var length in values) {
-    print(scream(length));
-  }
+  values.map(scream).forEach(print);
 }
