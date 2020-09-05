bb868b58d94679e51b19db65bd4f679cfd800e35
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-09-06 02:16:06 +0300

    .

---
+++ codeandart/dart01/sample10.dart
@@ -0,0 +1,14 @@
+// Conditional operators and loops
+void main() {
+  // if () {
+  //   // ...
+  // } else if () {
+  //   // ...
+  // } else {
+  //   // ...
+  // }
+
+  for (var i = 0; i < 5; i++) {
+    print(i);
+  }
+}
