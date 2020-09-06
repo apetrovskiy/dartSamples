56546dccc83eb0c123532502c8b6be33d2195249
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-09-06 19:13:30 +0300

    .

--- codeandart/dart01/sample10.dart
+++ codeandart/dart01/sample10.dart
@@ -8,7 +8,27 @@ void main() {
   //   // ...
   // }
 
+  print("for");
   for (var i = 0; i < 5; i++) {
     print(i);
   }
+
+  print("for in");
+  var collecton = [0, 1, 2];
+  for (var x in collecton) {
+    print(x);
+  }
+
+  print("for each");
+  collecton.forEach((element) {
+    print(element);
+  });
+
+  print("the forEach method");
+  var callbacks = [];
+  for (var i = 0; i < 2; i++) {
+    callbacks.add(() => print(i));
+  }
+
+  callbacks.forEach((value) => value());
 }
